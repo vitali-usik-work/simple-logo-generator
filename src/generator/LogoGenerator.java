18f8507feff499969558d0f3e9f188ce54e31c1e
vitali-usik-work <v.usik@godeltech.com>
2015-12-29 13:28:24 +0300

    generates correct logo

--- src/generator/LogoGenerator.java
+++ src/generator/LogoGenerator.java
@@ -15,102 +15,94 @@ import javax.imageio.ImageIO;
 public class LogoGenerator {
 
     public static void main(String[] args) {
-        doDemoResizeWithExistingFiles();
-        doDemoRisizeWithNewFiles();
+        String shopName = "S";
+        generateLogoImgFromShopName(shopName);
     }
     
-    private static void doDemoRisizeWithNewFiles() {
-        String text = "Success shop!";
-        /*
-           Because font metrics is based on a graphics context, we need to create
-           a small, temporary image so we can ascertain the width and height
-           of the final image
-         */
-        BufferedImage img = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2d = img.createGraphics();
+    private static void generateLogoImgFromShopName(String shopName) {
+        int logoWidth = 200;
+        int logoHeight = 132;
+        float transparentValue = 1.0f;
+        int marginX = 0, marginY = 0;
+        BufferedImage tempShopImg = prepareTextLogoWithShopName(shopName);
+        
+        int imgType = tempShopImg.getType() == 0? BufferedImage.TYPE_INT_ARGB : tempShopImg.getType();
+        BufferedImage resizeImageJpg;
+        if (tempShopImg.getWidth() > 200) {
+            resizeImageJpg = resizeImage(tempShopImg, imgType);
+            marginY = (logoHeight - resizeImageJpg.getHeight()) / 2;
+        } else {
+            resizeImageJpg = tempShopImg;
+            marginY = (logoHeight - resizeImageJpg.getHeight()) / 2;
+            marginX = (logoWidth - resizeImageJpg.getWidth()) / 2;
+        }
+        
+        BufferedImage backgroundImg = new BufferedImage(logoWidth, logoHeight, BufferedImage.TYPE_INT_ARGB);
+
+        BufferedImage logo = printTextLogoOnBackground(backgroundImg, resizeImageJpg, transparentValue, marginX, marginY);
+        
+        try {
+            ImageIO.write(logo, "png", new File("d:\\uspech2.png"));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    private static BufferedImage printTextLogoOnBackground(BufferedImage backgroundImg, BufferedImage resizeImageJpg, float transparentValue, int x,
+            int y) {
+        Graphics2D g2d = backgroundImg.createGraphics();
+        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, transparentValue));
+        g2d.drawImage(resizeImageJpg, x, y, null);
+        g2d.dispose();
+        return backgroundImg;
+    }
+
+    private static BufferedImage prepareTextLogoWithShopName(String shopName) {
+        BufferedImage shopNameImg = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = shopNameImg.createGraphics();
         Font font = new Font("Arial", Font.PLAIN, 48);
         g2d.setFont(font);
         FontMetrics fm = g2d.getFontMetrics();
-        int width = fm.stringWidth(text);
+        int width = fm.stringWidth(shopName);
         int height = fm.getHeight();
         g2d.dispose();
 
-        img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        g2d = img.createGraphics();
-//        g2d.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
-//        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-//        g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
-//        g2d.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
-//        g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
-//        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-//        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-//        g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
+        shopNameImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        g2d = shopNameImg.createGraphics();
+        g2d.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+        g2d.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+        g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
         g2d.setFont(font);
         fm = g2d.getFontMetrics();
         g2d.setColor(Color.BLACK);
-        g2d.drawString(text, 0, fm.getAscent());
+        g2d.drawString(shopName, 0, fm.getAscent());
         g2d.dispose();
         
-        try {
-            ImageIO.write(img, "png", new File("d:\\Text.png"));
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-        
-        try{
-            BufferedImage bgImage = ImageIO.read(new File("d:\\green.png"));
-            //BufferedImage originalImage = ImageIO.read(new File("d:\\space.jpg"));
-            int type = img.getType() == 0? BufferedImage.TYPE_INT_ARGB : img.getType();
-
-            BufferedImage resizeImageJpg = resizeImage(img, type);
-
-            float f = 1.0f;
-            int margin = (bgImage.getHeight() - resizeImageJpg.getHeight()) / 2;
-            BufferedImage newImage = addImage(bgImage, resizeImageJpg, f, 0, margin);
-            ImageIO.write(newImage, "png", new File("d:\\Text1.png"));                    
-        }catch(IOException e){
-            System.out.println(e.getMessage());
-        }
-    }
-
-    private static void doDemoResizeWithExistingFiles() {
-        try{
-            BufferedImage bgImage = ImageIO.read(new File("d:\\green.png"));
-            BufferedImage originalImage = ImageIO.read(new File("d:\\space.jpg"));
-            int type = originalImage.getType() == 0? BufferedImage.TYPE_INT_ARGB : originalImage.getType();
-
-            BufferedImage resizeImageJpg = resizeImage(originalImage, type);
-
-            float f = 1.0f;
-            int margin = (bgImage.getHeight() - resizeImageJpg.getHeight()) / 2;
-            BufferedImage newImage = addImage(bgImage, resizeImageJpg, f, 0, margin);
-            ImageIO.write(newImage, "png", new File("d:\\yyaaahooo.png"));                    
-        }catch(IOException e){
-            System.out.println(e.getMessage());
-        }
-    }
-
-    private static BufferedImage resizeImage(BufferedImage originalImage, int type){
-        float k = originalImage.getWidth() / 200;
-        System.out.println(k);
-        int h = (int) (originalImage.getHeight() / k);
-        System.out.println(h);
-        BufferedImage resizedImage = new BufferedImage(200, h, type);
-        Graphics2D g = resizedImage.createGraphics();
-        g.drawImage(originalImage, 0, 0, 200, h, null);
-        g.dispose();
-
-        return resizedImage;
+        return shopNameImg;
     }
     
-    /**
-     * prints the contents of buff2 on buff1 with the given opaque value.
-     */
-    private static BufferedImage addImage(BufferedImage buff1, BufferedImage buff2, float opaque, int x, int y) {
-        Graphics2D g2d = buff1.createGraphics();
-        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, opaque));
-        g2d.drawImage(buff2, x, y, null);
+    private static BufferedImage resizeImage(BufferedImage originalImage, int type){
+        float resizeFactor = originalImage.getWidth() / 200;
+        int newHeight = (int) (originalImage.getHeight() / resizeFactor);
+        BufferedImage resizedImage = new BufferedImage(200, newHeight, type);
+        Graphics2D g2d = resizedImage.createGraphics();
+        g2d.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+        g2d.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+        g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
+        g2d.drawImage(originalImage, 0, 0, 200, newHeight, null);
         g2d.dispose();
-        return buff1;
+
+        return resizedImage;
     }
 }
